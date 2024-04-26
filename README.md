54e772ee25a8b186a12d6b8fe4da27358cad0b39
Mark Schenk <m.m.a.schenk@gmail.com>
2024-04-26 08:54:47 +0200

    Test releasing

--- README.md
+++ README.md
@@ -3,10 +3,16 @@
 This repository helps you install a TU Delft specific ubuntu image for wsl. It downloads and registers the image, allowing you to run wsl with a number of pre-installed tools available. Among these tools are:
 - irods command line clients
 
-#3 Quick install
+## quick install
 
 Make sure you have the "Windows Subsystem for Linux" system component installed.
 
-## git config
+Open a powershell window and copy-and-paste the following line into it:
 
-Always run `git config --global core.sshcommand "C:/Windows/System32/OpenSSH/ssh.exe"` to set the ssh executable for all invocations of git (also from within vs-code)
+```
+ (New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mmaschenk/tudelft-wsl/releases/latest/download/tudinstaller.ps1') | iex
+```
+
+Wait for the image to be downloaded and registered. After this you can start `wsl -d tudelft` to run the image (or find it in the profiles of your Windows Terminal app)
+
+In case you do not trust me (you probably shouldn't), first download the [tudinstaller.ps1 script](https://raw.githubusercontent.com/mmaschenk/tudelft-wsl/releases/latest/download/tudinstaller.ps1) to your local machine and inspect what it does before running it!
---
+++ tudinstaller.ps1
@@ -0,0 +1,97 @@
+param (
+    $downloadurl = "https://github.com/mmaschenk/tudelft-wsl/releases/latest/download/tudelft.tgz"
+)
+
+function Update-ImageCacheFile {
+    param (
+        [Parameter(Position=0, Mandatory=$true)] $downloadurl,
+        $storename = 'wsl',
+        $cachepath = '.cache',
+        $base = $env:UserProfile
+    )
+    
+    if (-not [System.IO.Path]::IsPathRooted($storename)) {
+        $storename = Join-Path -Path $base -ChildPath $storename
+    }
+    if (-not [System.IO.Path]::IsPathRooted($cachepath)) {
+        $cachepath = Join-Path -Path $storename -ChildPath $cachepath
+    }
+    $basename = $downloadurl.Substring($downloadurl.LastIndexOf("/") + 1)
+    $imagefile = Join-Path -Path $cachepath -ChildPath $basename
+    $hashurl = "${downloadurl}.hash"
+
+    Write-Host "Downloadurl:   ", $downloadurl
+    Write-Host "Base is:       ", $base
+    Write-Host "Storename:     ", $storename
+    Write-Host "Cache path:    ", $cachepath
+    Write-Host "Imagefile:     ", $imagefile
+    Write-Host "Hashurl:       ", $hashurl
+
+    New-Item -ItemType Directory -Path $cachepath -ErrorAction SilentlyContinue
+
+    if (Test-Path -Path $imagefile -PathType leaf) {
+        Write-Host "* Image file already present"
+        $localhashvalue = (Get-FileHash $imagefile).Hash.Trim()
+    } else {
+        Write-Host "* Image file not yet present"
+        $localhashvalue = "NOTPRESENT"
+    }
+
+    $remotehash = Invoke-WebRequest -URI $hashurl
+
+    $remotehashvalue = [System.Text.Encoding]::ASCII.GetString($remotehash.Content).Trim()
+    Write-Host("Local hash value:  [{0}]" -f $localhashvalue)
+    Write-Host("Remote hash value: [{0}]" -f $remotehashvalue)
+
+    if ($remotehashvalue -ne $localhashvalue) {
+        Write-Host "* Need to download image file"
+        $webclient = New-Object System.Net.WebClient
+        Write-Host "* Downloading image file"
+        $webclient.DownloadFile($downloadurl, $imagefile.ToString())
+        Write-Host "* Imagefile downloaded"
+    }
+    return $imagefile
+}
+
+function Initialize-WSLStoreLocation {
+    param(
+        [Parameter(Position=0, Mandatory=$true)] $distribution,
+        $storename = 'wsl',
+        $base = $env:UserProfile
+    )
+
+    if (-not [System.IO.Path]::IsPathRooted($storename)) {
+        $storename = Join-Path -Path $base -ChildPath $storename
+    }
+
+    $fullstorepath = Join-Path -Path $storename -ChildPath $distribution
+
+    if (Test-Path $fullstorepath) {
+        throw ("Directory {0} already exists." -f $fullstorepath)
+    }
+    $ni = New-Item -Path $fullstorepath -ItemType Directory 
+    Write-Host("Created fullstorepath: {0} [{1}]" -f $fullstorepath, $fullstorepath.getType())
+
+    return $fullstorepath.ToString()
+}
+
+function  Register-ImageFile {
+    param (
+        [Parameter(Position=0, Mandatory=$true)] $downloadurl,
+        $registrationname = 'tudelft'
+    )
+
+    $cachefile = Update-ImageCacheFile($downloadurl)
+    $fullstorepath = Initialize-WSLStoreLocation($registrationname)
+
+    Write-Host("cachefile:       : {0} [{1}]" -f $cachefile, $cachefile.getType())
+    Write-Host("registration name: {0} [{1}]" -f $registrationname, $registrationname.getType())
+    Write-Host("storepath:         {0} [{1}]" -f $fullstorepath, $fullstorepath.getType())
+
+    Write-Host("going to run: wsl.exe --import {0} {1} {2}" -f $registrationname,"$fullstorepath","$cachefile")
+    wsl.exe --import $registrationname "$fullstorepath" "$cachefile"
+}
+
+
+Write-Host("Installing:  {0}" -f $downloadurl)
+Register-ImageFile -downloadurl $downloadurl
